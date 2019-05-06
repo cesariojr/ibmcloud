20a1b2535171e8462e11e8735aeb117465689142
Danilo de Novais Silveira <coragi@gmail.com>
2019-05-06 11:33:14 -0300

    limpeza e favicon

--- src/app/mainscreen/mainscreen.component.ts
+++ src/app/mainscreen/mainscreen.component.ts
@@ -14,22 +14,16 @@ export class MainscreenComponent implements OnInit {
   constructor(private messageService: MessageService) { }
 
   ngOnInit() {
-
   }
 
   start(parametro: number) {
-    // this.playerOne.name = "Danilo";
     this.getRooms(parametro);
   }
 
   getRooms(parametro: number): void {
     this.messageService.getRooms(parametro)
       .subscribe(rooms => {
-        console.log('result:', rooms);
         this.room = rooms;
-        console.log('this.rooms', this.room);
-        console.log('this.rooms.id', this.room.id);
-        //console.log('this.rooms.id', this.rooms.id);
       });
   }
 }
--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -13,7 +13,6 @@ export class MessageService {
   constructor(private http: HttpClient) { }
 
   getRooms(parametro: number): Observable<Room> {
-    //return of(ROOMS);
     var url: string;
     url = "https://us-south.functions.cloud.ibm.com/api/v1/web/dnovais%40br.ibm.com_dev-Danilo/ibm.cloud.qcon/read-from-cloudant.json?id=" + parametro;
     console.log("url", url);
